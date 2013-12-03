a903bd579d76396ffae2b85b9c27e86444c35fed
casa <casa@192.168.1.40>
2013-12-03 12:03:38 -0200

    Adiciona HibernateUtil

---
+++ portalav3/src/main/java/com/portalav3/model/dao/InterfaceDAO.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.portalav3.model.dao;
+
+/**
+ *
+ * @author casa
+ */
+public interface InterfaceDAO<T> {
+    
+    
+}
